e1f2c7d0a655d1603b96ea24a39462cc74be2abf
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-10-30 11:55:45 +0000

    wip 5a

--- exercise_5.1.py
+++ exercise_5.1.py
@@ -11,6 +11,17 @@ def exercise_2a():
     
     print(list)
 
-exercise_2a()
+#exercise_2a()
 
+#5a) Find the average of marks in 3 subjects for a 1000 students. The students' id range from 0 to 999. 
+# The marks can be generated randomly. Implement this using both lists and dictionaries. 
+# Compare the size (of the data structures) in memory as well as performance (ie, running time), and comment on your findings.
+
+def exercise_5a():
+    student_id = []
+    for i in range(1000):
+        student_id.append(i)
+    print(student_id)
+
+exercise_5a()
     
