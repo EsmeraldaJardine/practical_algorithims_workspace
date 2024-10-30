1d58fa359028627543b2c57aa54fa5f948c21f74
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-10-30 18:15:23 +0000

    1a wip

--- exercise_4.1.py
+++ exercise_4.1.py
@@ -6,15 +6,16 @@
 
 def exercise_1a():
     def get_inputs():
-        price= int(input("give the before tax price: "))
-        discount = int(input ("give the discount value: "))
+        price= float(input("give the before tax price as a float: "))
+        discount = int(input ("give the discount percentage as a float (1-100): "))
         return price, discount
     
     def get_new_price(price, discount):
-        return print(round(price*(1-discount/100)*1.175, 2))
- 
+        return print(f"post tax price with discount: £{round(price*(1-discount/100)*1.175, 2)}")
+
     price, discount = get_inputs()
     get_new_price(price, discount)
+
 exercise_1a()
 
 
