3ad4d924e5d901bfed220f5e5755912178914642
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-10-30 19:12:18 +0000

    1b

--- exercise_4.1.py
+++ exercise_4.1.py
@@ -1,9 +1,9 @@
+import random
 #The algorithmic building blocks
 
 #1a) Write a Python program that prompts for two inputs: 
 # the before tax price of an item, and the discount offered on it (as a percentage). 
 # The program should then calculate and display the new price, given that the sales tax rate is 17.5%.
-
 def exercise_1a():
     def get_inputs():
         price= float(input("give the before tax price as a float: "))
@@ -16,6 +16,22 @@ def exercise_1a():
     price, discount = get_inputs()
     get_new_price(price, discount)
 
-exercise_1a()
+#exercise_1a()
+
+
+# Write a program that prompts the user to enter their last name, 
+# and then creates a 6 character user id based on the first three letters 
+# of their surname (in lower case), and a 3 digit randomly generated number.
+
+def exercise_1b():
+    last_name = list(input("enter your last name: ").lower())
+    random_digit = random.choices('0123456789', k=3)
+
+    user_id = ''.join(last_name[0:3]) + ''.join(random_digit)
+    return print(f"user_id: {user_id}")
+
+#exercise_1b()
+
+
 
 
